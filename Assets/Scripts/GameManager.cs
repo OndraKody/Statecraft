7fa30bfc36d4093918ac99ff43768df4306677a2
OndraKody <ondrejkodes4@gmail.com>
2025-10-22 20:26:29 +0200

    rijen_2ukol

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance; 
+
+    public Party selectedParty;
+    public double expenses;
+    public double income;
+    public double dept;
+    public string gamePhase; 
+
+    
+
+    private void Awake()
+    {
+        
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject); 
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Party.cs
@@ -0,0 +1,12 @@
+using System;
+
+[System.Serializable]
+public class Party
+{
+    public string name;          
+    public string ideology;
+    public string[] secundery_ideology;
+    public int power;
+    public int seats;
+    public string[] goals;       
+}
